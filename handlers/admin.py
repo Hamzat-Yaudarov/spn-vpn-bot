c1851b874744d500bb8a63052524d74eaf862566
g3638114-glitch <g3638114@gmail.com>
2026-05-18 18:58:38 +0400

    Новые тарифы

--- handlers/admin.py
+++ handlers/admin.py
@@ -782,8 +782,6 @@ async def handle_broadcast_no_sub_message(message: Message, state: FSMContext):
                 WHERE NOT EXISTS (
                     SELECT 1 FROM subscriptions
                     WHERE subscriptions.tg_id = users.tg_id
-                      AND subscriptions.generation = 'v2'
-                      AND subscriptions.is_visible = TRUE
                       AND subscriptions.subscription_until IS NOT NULL
                       AND subscriptions.subscription_until > now() AT TIME ZONE 'UTC'
                 )
